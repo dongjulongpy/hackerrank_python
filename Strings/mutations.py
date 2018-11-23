04c7b74aad8f41fa46a028dc5fe138b5b6fb6003
Dong Ju-Long <44946009+dongjulongpy@users.noreply.github.com>
2018-11-23 14:46:34 +0300

    Update mutations.py

--- Strings/mutations.py
+++ Strings/mutations.py
@@ -1,5 +1,5 @@
 def mutate_string(string, position, character):
-    string_list = list(string)[position] = character
+    string_list = list(string)
     string_list[position] = character
     return "".join(string_list)
 
@@ -7,4 +7,3 @@ if __name__ == '__main__':
     s = input()
     i, c = input().split()
     s_new = mutate_string(s, int(i), c)
-    print(s_new)
